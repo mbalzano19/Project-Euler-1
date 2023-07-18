f9a0746d06f3e8c0446f464663da893c2b501ef5
mbalzano19 <125160521+mbalzano19@users.noreply.github.com>
2023-07-18 13:35:46 -0400

    pushes answer code for Project Euler Question #1

---
+++ main.js
@@ -0,0 +1,13 @@
+// Project Euler Question #1
+
+function multiples() {
+    let sum = 0
+    for (let i = 0; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum = sum + i
+        }
+    }
+    return sum
+}
+
+console.log(multiples())
